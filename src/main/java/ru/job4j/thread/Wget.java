6cd52efc8e62807d6ff9e0412a2b6e423309df31
VladimirKozemaslov <kvs@intelins.ru>
2023-04-28 19:23:16 +0500

    8. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -24,19 +24,25 @@ public class Wget implements Runnable {
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
              FileOutputStream fileOutputStream = new FileOutputStream(destPath)) {
-            byte[] dataBuffer = new byte[speed];
+            int stepSize = Math.min(speed, 100);
+            byte[] dataBuffer = new byte[stepSize];
             int bytesRead;
+            int totalRead = 0;
             long actualTime;
             Instant finish;
             Instant start = Instant.now();
-            while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
-                finish = Instant.now();
-                actualTime = Duration.between(start, finish).toMillis();
-                if (bytesRead == speed && actualTime < 1000) {
-                    Thread.sleep(1000 - actualTime);
+            while ((bytesRead = in.read(dataBuffer, 0, stepSize)) != -1) {
+                totalRead += bytesRead;
+                if (speed <= totalRead) {
+                    finish = Instant.now();
+                    actualTime = Duration.between(start, finish).toMillis();
+                    if (actualTime < 1000) {
+                        Thread.sleep(1000 - actualTime);
+                    }
+                    totalRead = 0;
+                    start = Instant.now();
                 }
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                start = Instant.now();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -46,11 +52,10 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        for (String arg : args) {
-            if (arg == null) {
-                throw new IllegalArgumentException();
-            }
+        if (args.length != 3) {
+            throw new IllegalArgumentException();
         }
+
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         String dest = args[2];
