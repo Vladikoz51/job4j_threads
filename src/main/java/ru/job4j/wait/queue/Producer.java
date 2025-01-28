3dcffe5786d90b41ce2bda728778018811eed4fc
VladimirKozemaslov <kvs@intelins.ru>
2025-01-28 15:29:15 +0500

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/main/java/ru/job4j/wait/queue/Producer.java
@@ -0,0 +1,19 @@
+package ru.job4j.wait.queue;
+
+import java.util.Random;
+
+public class Producer implements Runnable {
+    private final SimpleBlockingQueue<Integer> queue;
+
+    public Producer(SimpleBlockingQueue<Integer> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        while (!queue.isFull()) {
+            queue.offer(random.nextInt());
+        }
+    }
+}
